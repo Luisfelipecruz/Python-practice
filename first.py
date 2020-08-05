c658f6763f04e00b4f24daa048debddfaf034648
Luisfelipecruz <luisfelipecruzgeo@hotmail.com>
2020-08-04 19:15:56 -0500

    constant calculation of payment

---
+++ first.py
@@ -0,0 +1,8 @@
+# This first line is provided for you
+
+hrs = input("Enter Hours:")
+pay = float(hrs)
+rate = input("Enter the rate per hour:")
+rate = float(rate)
+rate = rate*pay
+print('Pay:',rate)
